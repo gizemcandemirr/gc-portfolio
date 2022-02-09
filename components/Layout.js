fa52cacb1b25abce235190e51ff42a04e5115f77
gizem <gizem@DESKTOP-5MUKOCP>
2022-02-09 22:11:40 +0300

    Portfolio-React-Next-GraphCMS

---
+++ components/Layout.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Head from 'next/head'
+import Header from './Header'
+
+const Layout = ({title, description, children}) =>{
+
+    return (
+        <div>
+         <Head>
+        <title>{title}</title>
+        <meta name="description" content={description}/>
+        <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className="relative min-h-screen bg-primary-light font-montserrat">
+            <div className=" z-10 px-3 overflow-hidden w-full h-full">
+                <Header/>
+                 {children}
+            </div>
+           
+        </div>
+                
+        </div>
+    )
+}
+
+export default Layout
