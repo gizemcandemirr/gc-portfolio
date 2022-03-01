7cf7367a462d38f980bfd1e011c9a5938099bda0
Gizem <gizem.candemir@secilstore.com>
2022-03-01 13:37:45 +0300

    continue...

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <div >
       <Layout title={"Portfolio- Gizem Candemir"} description={"Gizem Candemir Portfolio"}>
-          <motion.img src="/images/ball-1.svg" alt="ball-1" className="hidden md:block h-auto absolute top-48 right-10 z-[1] "
+          <motion.img src="/images/blue-ball.png" alt="ball-1" className="w-44 hidden md:block h-auto absolute top-48 right-10 z-[1] "
            whileHover={{y:-10, rotate:90, transition: {duration: 0.4}}}
            drag
            dragConstraints={{top:0, left:0, right:0, bottom:0}}    
           />
-          <motion.img src="/images/ball-2.svg" alt="ball-2" className="hidden md:block h-auto absolute bottom-10 left-10 z-[1] "
+          <motion.img src="/images/blue-ball.png" alt="ball-2" className="w-36 hidden md:block h-auto absolute bottom-10 left-10 z-[1] "
           whileHover={{y:-10, rotate:90, transition: {duration: 0.4}}}  
           drag
           dragConstraints={{top:0, left:0, right:0, bottom:0}}        />
