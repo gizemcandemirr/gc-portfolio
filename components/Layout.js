f5b7ddef44a636efca7f94f549a6bbb85904143c
gizem <gizem@DESKTOP-5MUKOCP>
2022-04-03 10:18:59 +0300

    added buy me a coffee widget

--- components/Layout.js
+++ components/Layout.js
@@ -12,6 +12,7 @@ const Layout = ({title, description, children}) =>{
         <title>{title}</title>
         <meta name="description" content={description}/>
         <link rel="icon" href="/favicon.ico" />
+        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="mhndsbgyn" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF5F5F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
         </Head>
         <div className="relative min-h-screen bg-primary-light font-montserrat dark:bg-gray-900 dark:text-white">
             <div className=" z-10 px-3 overflow-hidden w-full h-full">
